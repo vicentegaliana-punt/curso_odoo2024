43f2ffce5e40c44a515fbba65e313cf398c92c32
Vicente Galiana <vicentegaliana@gmail.com>
2024-09-15 17:32:46 +0000

    tras crear primer modelo

---
+++ sports_association/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sports Association",
+    "summary": "Manage sports association members, teams and events",
+    "version": "17.0.1.0.0",
+    "category": "Sports",
+    "author": "<Vicente Galiana>, Punt Sitemes",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+
+    ],
+}
